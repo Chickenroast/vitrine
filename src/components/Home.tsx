14ce87285542a18dfbb850d9937a6211d36ac0cc
Julia Michel T. <juliamicheltex@gmail.com>
2023-12-28 22:23:47 +0100

    home

---
+++ src/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="h-screen">
+      <div className="flex h-80 bg-white rounded-lg">
+        <div className="">
+          <p className="text-start mx-5 mt-5 text-lg">Julia Michel T.</p>
+          <h1 className="text-center mt-10 text-5xl font-bold">
+            FRONT END DEVELOPPER
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
