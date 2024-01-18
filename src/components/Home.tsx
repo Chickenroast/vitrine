9bcdd589e2375ddf15b754168e2c35c6d0226504
Julia Michel T. <juliamicheltex@gmail.com>
2024-01-18 09:41:21 +0100

    saves

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,22 +2,24 @@ import React from "react";
 import ScrollDownCircle from "./ScrollDown";
 const Home: React.FC = () => {
   return (
-    <div className="h-screen flex items-center">
-      <div className="flex p-10 h-fit bg-white rounded-lg overflow-hidden">
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <div className="flex lg:p-10 h-[90%] lg:h-fit bg-white rounded-lg overflow-hidden">
         <div className="p-5">
           <div className="flex">
-            <p className="mx-5 mt-5 text-lg">Julia Michel T.</p>
-            <div className="ml-auto flex space-x-5 mt-5 text-lg">
+            <div className="text-xs ml-auto flex space-x-5 mt-5 lg:text-lg">
               <p>Projects</p>
               <p>About</p>
               <p>Contact</p>
             </div>
           </div>
-          <h1 className="text-left mx-5 mt-10 text-7xl font-bold font-CutoffPro">
+          <p className="text-left lg:mx-5 mt-[50%] lg:text-lg">
+            Julia Michel T.
+          </p>
+          <h1 className="text-4xl text-left lg:mx-5 lg:mt-10 lg:text-7xl font-bold font-CutoffPro mt-5">
             FRONT END DEVELOPPER
           </h1>
-          <div className="mt-10 mb-5 flex items-center justify-center">
-            <div className="align-center">
+          <div className="mt-[50%] mb-5 flex items-center justify-center">
+            <div className="">
               <ScrollDownCircle />
             </div>
           </div>
